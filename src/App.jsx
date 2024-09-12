efe6b833127ea9aed9a1069cf91d4a807aad5b5a
Arthur Araujo <arthurtute200@hotmail.com>
2024-09-11 23:03:06 -0300

    feat: Created the application's Header, where the score will be displayed. When the wabpage first logs, the scores are null, when the button Initialize Game is clicked, the score for both players is updated to 0

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
+import Header from './Components/Header/Header'
 
 
 function App() {
- 
+ const [playerScore,setPlayerScore] = useState(null)
+ const [machineScore,setMachineScore] = useState (null) 
+ return(
+    <Header playerScore={playerScore} setPlayerScore={setPlayerScore} machineScore={machineScore} setMachineScore={setMachineScore}/>
+    )  
 }
 
 export default App
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,1 +1,19 @@
-// This is the application's header, where the score will be displayed
+export default function Header(props){
+    function handleInit(){
+        props.setPlayerScore(0)
+        props.setMachineScore(0)
+    }
+    return(
+        <>
+            <div>
+                <button type="button" id="initButton" onClick={() => handleInit()}>Initialize Game</button>
+            </div>
+            <div>
+                <label htmlFor="playerScore">Player Score:</label>
+                <input type="text" id="playerScore" value={props.playerScore} />
+                <label htmlFor="machineScore">Opponent Score:</label>
+                <input type="text" id="machineScore" value ={props.machineScore} />
+            </div>
+        </>
+    )
+}
